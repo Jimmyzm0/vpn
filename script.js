a6c1cb1e0a5b4bda7e58e2ddffbee1033e75b35a
Blues <115449600+Jimmyzm0@users.noreply.github.com>
2023-12-25 11:59:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     // 例如，加载图表或者其他动态数据
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const blessings = [
+        // 您的祝福语数组
+    ];
+
+    const container = document.getElementById('blessings-container');
+    const showMoreBtn = document.getElementById('show-more');
+
+    for (let i = 0; i < Math.min(blessings.length, 10); i++) {
+        const p = document.createElement('p');
+        p.textContent = blessings[i];
+        container.appendChild(p);
+    }
+
+    showMoreBtn.addEventListener('click', function() {
+        if (container.style.maxHeight) {
+            container.style.maxHeight = null;
+            this.textContent = '显示更多';
+        } else {
+            container.style.maxHeight = container.scrollHeight + 'px';
+            this.textContent = '显示较少';
+        }
+    });
+});
